b93a6783484a270139f0d3e357e3e93662c702ad
marvin <marvkipi@gmail.com>
2015-12-27 19:42:05 -0800

    initial files

---
+++ app.js
@@ -0,0 +1,23 @@
+var app = angular.module("flipperNews", []);
+
+app.controller("MainCtrl", [
+	'$scope',
+	function($scope) {
+		$scope.test = "Hello world!";
+		$scope.posts = [
+			{title:'post 1', upvotes: 5},
+			{title:'post 2', upvotes: 4},
+			{title:'post 3', upvotes: 15},
+			{title:'post 4', upvotes: 4},
+			{title:'post 5', upvotes: 5}
+		];
+		$scope.addPost = function() {
+			if(!$scope.title || $scope.title === ' ') { return; }
+			$scope.posts.push({title: $scope.title, upvotes: 0});
+			$scope.title = " ";
+		};
+
+		$scope.incrementUpvotes = function(post) {
+			post.upvotes++;
+		};
+}]);
